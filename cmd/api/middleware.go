0e0e8a2eb857ad3c5e14b2387ee4864c3a0671d4
victor alvarez rojas <victoralvarezrojas@Mac-Studio.local>
2026-07-19 19:26:52 -0400

    feat(users): send welcome email in background after registration

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -43,7 +43,7 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		defer func() {
 			if err := recover(); err != nil {
 				w.Header().Set("Connection", "close")
-				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+				app.serverErrorResponse(w, r, fmt.Errorf("error panic %s", err))
 			}
 		}()
 		next.ServeHTTP(w, r)
