14851f8a9c714cddefbcc4cda3a51d853d8e1ec8
victor alvarez rojas <victoralvarezrojas@Mac-Studio.local>
2026-08-05 12:12:33 -0400

    feat(permission): add permission model and query to permissions for user

--- README.md
+++ README.md
@@ -537,5 +537,7 @@ y, por ejemplo, lanzar un cierre ordenado del servidor HTTP usando `Shutdown()`.
 | 16.     | Permission-based Authorization.            | ✅ |
 | 16.1    | Requiring User Activation.                 | ✅ |
 | 16.2    | Setting up the Permissions Database Table  | ✅ |
+| 16.3    | Setting up the Permissions Mode.           | ✅ |
+
 ```
 
--- internal/data/models.go
+++ internal/data/models.go
@@ -11,15 +11,17 @@ var (
 )
 
 type Models struct {
-	Movies MovieModel
-	Usr    UserModel
-	Tkn    TokenModel
+	Movies      MovieModel
+	Usr         UserModel
+	Tkn         TokenModel
+	Permissions PermissionModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{BD: db},
-		Usr:    UserModel{DB: db},
-		Tkn:    TokenModel{DB: db},
+		Movies:      MovieModel{BD: db},
+		Usr:         UserModel{DB: db},
+		Tkn:         TokenModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
